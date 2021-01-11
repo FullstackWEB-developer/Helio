b17a6526d1ae9de5bfbae0fe2a7926a0ec6d361a
Serhiy Posokhin <serhiy.posokhin@authoritypartners.com>
2021-01-11 13:19:24 +0000

    Squashed commit of the following
    commit c28870ab 
    Author: Serhiy Posokhin <serhiy.posokhin@authoritypartners.com> 
    Date: Mon Jan 11 2021 15:12:20 GMT+0200 (Восточная Европа, стандартное время) 
    
        Disable debug. Remove comments.
    
    
    commit a9b6f7e0 
    Author: Serhiy Posokhin <serhiy.posokhin@authoritypartners.com> 
    Date: Mon Jan 11 2021 14:56:08 GMT+0200 (Восточная Европа, стандартное время) 
    
        Add i18n

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import {Switch, Route, BrowserRouter, Link} from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 import Tickets from '../pages/tickets/ticket-list';
 import './app.css';
 
 function App() {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (language: string) => {
+        i18n.changeLanguage(language);
+    };
+
   return (
       <BrowserRouter>
+          <div className={"row"}>
+              <div className={"col-4"}>
+                  <button onClick={() => changeLanguage('en')}>EN</button>
+                  <button onClick={() => changeLanguage('es')}>ES</button>
+                  <h1>{t('title')}</h1>
+                  <h2>{t('description.part1')}</h2>
+                  <p>{t('description.part2')}</p>
+              </div>
+          </div>
           <div className={"row"}>
               <div className={"col-2"}>
                   <nav>
