f02073dcbd094ac9606c40aa9672346f897e4bd6
Antonio Copic <antonio.copic@authoritypartners.com>
2021-01-14 10:57:38 +0000

    Squashed commit of the following
    commit b974feca 
    Author: acopic <antonio.copic@gmail.com> 
    Date: Thu Jan 14 2021 11:55:40 GMT+0100 (srednjoeuropsko standardno vrijeme) 
    
        436 - removed unneccessary code
    
    
    commit d6645476 
    Author: acopic <antonio.copic@gmail.com> 
    Date: Thu Jan 14 2021 11:47:15 GMT+0100 (srednjoeuropsko standardno vrijeme) 
    
        436 - Lazy loading the components and adding skeleton fallback loader

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { TicketsWithErrors } from '../pages/tickets/ticket-list';
+import {lazily} from "react-lazily";
+import FallbackLoader from '../shared/components/skeleton-loader/skeleton-loader';
 import Layout from '../shared/layout/layout';
+
+const { TicketsWithErrors } = lazily(() => import('../pages/tickets/ticket-list'))
+
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Layout>
-                    <Route path="/tickets">
-                        <TicketsWithErrors/>
-                    </Route>
+                    <Suspense fallback={<FallbackLoader />}>
+                        <Route path="/tickets">
+                            <TicketsWithErrors/>
+                        </Route>
+                    </Suspense>
                 </Layout>
             </Switch>
         </BrowserRouter>
