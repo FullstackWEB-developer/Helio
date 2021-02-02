7b5dcaef3bfa2bdad1a9565a936f978a2f682aea
FullstackWEB-developer <sgirad86@gmail.com>
2021-02-02 11:23:50 +0000

    Squashed commit of the following
    commit ef277955 
    Author: Burak Bolek <burak.bolek@authoritypartners.com> 
    Date: Tue Feb 02 2021 12:38:49 GMT+0300 (GMT+03:00) 
    
        Add MS Logout to logout

--- src/shared/layout/components/profile-dropdown.tsx
+++ src/shared/layout/components/profile-dropdown.tsx
@@ -2,13 +2,19 @@ import { Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { logOut } from '../../store/app-user/appuser.slice';
+import { msalInstance } from "../../../pages/login/auth-config";
 import { ReactComponent as PlaceholderIcon } from '../../icons/Icon-Placeholder-24px.svg';
 const ProfileDropdown = () => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
+
+    const logout = () => {
+        dispatch(logOut());
+        msalInstance.logout();
+    }
     return (<Fragment>
         <div className="cursor-pointer absolute bg-white mt-2 ring-black right-0 ring-1 ring-opacity-5 rounded-md shadow-lg w-56">
-            <div onClick={() => dispatch(logOut())} className="p-4 flex flex-row">
+            <div onClick={() => logout()} className="p-4 flex flex-row">
                 <div className="px-2">
                     <PlaceholderIcon></PlaceholderIcon>
                 </div>
