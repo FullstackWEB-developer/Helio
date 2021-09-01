ba39fc96169da3f3d3440bb09b8f4c33a3e66ed2
Martín José Acosta Santos <martin.acosta@authoritypartners.com>
2021-09-01 07:10:31 +0000

    Product Backlog Item 2599: MDLIC - Adding API Keys to API Gateway - Backend & Frontend

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -14,6 +14,7 @@ const Api = axios.create({
 
 Api.interceptors.request.use(async (config) => {
     config.headers['x-api-challenge'] = localStorage.getItem('challenge') || 'no-key-found';
+    config.headers['X-Api-Key'] = process.env.REACT_APP_AWS_API_KEY;
     const token = await refreshAccessToken();
     if (token) {
         config.headers.Authorization = token;
