de3551c213e6d1559a40b721213ae4383f81f022
FullstackWEB-developer <sgirad86@gmail.com>
2022-03-11 09:53:00 +0000

    Bug 5033: Wrong data display / data missing

--- src/shared/components/dashboard/helio-responsive-line/helio-responsive-line.tsx
+++ src/shared/components/dashboard/helio-responsive-line/helio-responsive-line.tsx
@@ -1,7 +1,8 @@
 import {DashboardColors} from '@pages/dashboard/utils/dashboard-utils';
 import {ChartTooltip, CustomTick} from '@components/dashboard';
 import {Point, ResponsiveLine, Serie} from '@nivo/line'
-import {useMemo} from 'react';
+import {useEffect, useMemo, useState} from 'react';
+import {useTranslation} from 'react-i18next';
 
 export interface HelioResponsiveLineProps {
     data: Serie[];
@@ -10,9 +11,24 @@ export interface HelioResponsiveLineProps {
 }
 const HelioResponsiveLine = ({data, tickRotation, toolTipLabelGenerator} : HelioResponsiveLineProps) => {
 
+    const [hasData, setHasData] = useState<boolean>(false);
+    const {t} = useTranslation();
+
+    useEffect(() => {
+        data.forEach(series => {
+           if (series.data.length > 0) {
+               setHasData(true);
+           }
+        });
+    }, [data]);
+
     const style = useMemo(() => {
         return getComputedStyle(document.body);
     }, []);
+
+    if (!hasData) {
+        return <div className='flex h-full w-full items-center justify-center body3-medium'>{t('my_stats.calls_chats.no_data')}</div>
+    }
 
     return <ResponsiveLine
         data={data}
