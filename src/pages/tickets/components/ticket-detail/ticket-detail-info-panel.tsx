40eaea48f8c675f737be895e22ac2b7fdafd5e67
Amir Lisovac <amir.lisovac@authoritypartners.com>
2022-03-21 13:03:44 +0000

    Bug 5962: Scrollable content is overlapping title (Details)

--- src/pages/tickets/components/ticket-detail/ticket-detail-info-panel.tsx
+++ src/pages/tickets/components/ticket-detail/ticket-detail-info-panel.tsx
@@ -173,7 +173,7 @@ const TicketDetailInfoPanel = ({ticket, patient, contact}: TicketDetailInfoPanel
 
     return <>
         <form className='relative flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-            <div className='sticky top-0 flex items-center justify-between px-6 ticket-details-info-header'>
+            <div className='sticky top-0 flex items-center justify-between px-6 ticket-details-info-header z-30'>
                 <h6>{t('ticket_detail.info_panel.details')}</h6>
                 {
                     isDirty() &&
