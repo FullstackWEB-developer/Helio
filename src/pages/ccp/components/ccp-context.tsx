5096c11890f75f569010cbcb5bef2e0c3b44c76a
Amir Lisovac <amir.lisovac@authoritypartners.com>
2022-04-06 14:03:24 +0000

    Product Backlog Item 6232: MDLIC - Internal Calls: Adding Caller Name into Bot Context - Frontend & Connect

--- src/pages/ccp/components/ccp-context.tsx
+++ src/pages/ccp/components/ccp-context.tsx
@@ -1,10 +1,9 @@
 import React, {useMemo} from 'react';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 import withErrorLogging from '../../../shared/HOC/with-error-logging';
 import BotContext from './bot-context';
 import NoteContext from './note-context';
 import contextPanels from '../models/context-panels';
-
 import {
     selectBotContext,
     selectContextPanel,
@@ -16,29 +15,103 @@ import {ContextKeyValuePair} from '@pages/ccp/models/context-key-value-pair';
 import SvgIcon, {Icon} from '@components/svg-icon';
 import utils from '@shared/utils/utils';
 import ExtensionsContext from '@pages/ccp/components/extensions-context';
+import {useTranslation} from 'react-i18next';
+import {Intent} from '../models/intent.enum';
 
 const CcpContext = () => {
+    const {t} = useTranslation();
     const context = useSelector(selectContextPanel);
     const hasActiveContact = useSelector(selectHasActiveContact);
     const patientInCollectionsBalance = useSelector(selectPatientInCollectionsBalance);
     const botContext = useSelector(selectBotContext);
 
-    const botContextMessages: ContextKeyValuePair[] = useMemo(() => {
-        const items =  [
-            {
-                label : 'ccp.bot_context.queue',
-                value: botContext?.queue
-            },
-            {
-                label: 'ccp.bot_context.reason',
-                value: botContext?.reason
-            }
-        ] as ContextKeyValuePair[];
+    const determineCallerName = () => {
+        const patientFullNameAttribute = checkConnectAttributesForValue('PatientFullName');
+        const initialInputNameAttribute = checkConnectAttributesForValue('InitialInputName');
+        const createdForName = checkConnectAttributesForValue('CreatedForName');
+        const incomingPhoneNumber = checkConnectAttributesForValue('IncomingPhoneNumber');
+        const isInternalCall = checkConnectAttributesForValue('IsInternalCall');
+        const internalCallFromUserName = checkConnectAttributesForValue('FromUserName');
 
         if (botContext?.patient) {
+            return utils.stringJoin(' ', botContext.patient.firstName, botContext.patient.lastName);
+        }
+        if (patientFullNameAttribute) {
+            return patientFullNameAttribute;
+        }
+        if (botContext?.contactId) {
+            if (createdForName) {
+                return createdForName;
+            }
+            const welcomeVoiceName = checkConnectAttributesForValue('WelcomeName');
+            if (welcomeVoiceName) {
+                return welcomeVoiceName;
+            }
+        }
+        if (isInternalCall && Boolean(isInternalCall.toLowerCase()) === Boolean("true") && internalCallFromUserName) {
+            return internalCallFromUserName;
+        }
+        if (createdForName) {
+            return createdForName;
+        }
+        if (initialInputNameAttribute) {
+            return initialInputNameAttribute;
+        }
+        if (incomingPhoneNumber) {
+            return incomingPhoneNumber;
+        }
+        return '';
+    }
+
+    const determineCallerType = () => {
+        const isInternalCall = checkConnectAttributesForValue('IsInternalCall');
+        const internalCallFromUserName = checkConnectAttributesForValue('FromUserName');
+        if (botContext?.patient) {
+            return t('ccp.bot_context.patient');
+        }
+        if (botContext?.contactId) {
+            return t('ccp.bot_context.contact');
+        }
+        if (isInternalCall && Boolean(isInternalCall.toLowerCase()) === Boolean("true") && internalCallFromUserName) {
+            return t('ccp.bot_context.internal');
+        }
+        return '';
+    }
+
+    const checkConnectAttributesForValue = (key: string) => {
+        return botContext?.attributes?.find(a => a.label === key)?.value;
+    }
+
+    const determineCallerReason = () => {
+        if (!botContext?.reason || botContext.reason === 'none') {return };
+        const recognizedReasonFromIntent = Intent[botContext.reason];
+        return recognizedReasonFromIntent ?? utils.spaceBetweenCamelCaseWords(botContext.reason);
+    }
+
+    const botContextMessages: ContextKeyValuePair[] = useMemo(() => {
+        const items: ContextKeyValuePair[] = [];
+
+        if (botContext?.queue) {
             items.push({
-                label: 'ccp.bot_context.patient_name',
-                value: utils.stringJoin(' ', botContext.patient.firstName, botContext.patient.lastName)
+                label: 'ccp.bot_context.queue',
+                value: botContext?.queue
+            })
+        }
+
+        const callerName = determineCallerName();
+        if (callerName) {
+            const callerType = determineCallerType();
+            items.push({
+                label: 'ccp.bot_context.caller',
+                value: `${callerName}${callerType ? ` (${callerType})` : ''}`
+            });
+        }
+
+        const reason = determineCallerReason();
+        if (reason) {
+            items.push({
+                label: 'ccp.bot_context.reason',
+                value: reason
             });
         }
 
@@ -61,7 +134,7 @@ const CcpContext = () => {
     const renderContext = () => {
         switch (context) {
             case contextPanels.bot:
-                return <BotContext items={botContextMessages}/>
+                return <BotContext items={botContextMessages} />
             case contextPanels.note:
                 return <NoteContext />
             case contextPanels.sms:
@@ -78,17 +151,17 @@ const CcpContext = () => {
             return false;
         }
         return hasActiveContact || context === contextPanels.extensions
-    }, [context, hasActiveContact ])
+    }, [context, hasActiveContact])
 
     if (!displayContext) {
         return null;
     }
     return <div className={'ccp-context flex flex-col'}>
-            <div className={'ccp-header'} />
-            <div className={'ccp-main-content flex-grow bg-white border-l'}>
-                {renderContext()}
-            </div>
+        <div className={'ccp-header'} />
+        <div className={'ccp-main-content flex-grow bg-white border-l'}>
+            {renderContext()}
         </div>
+    </div>
 }
 
 export default withErrorLogging(CcpContext);
---
+++ src/pages/ccp/models/intent.enum.ts
@@ -0,0 +1,14 @@
+export enum Intent {
+    RequestRXRefill = 'Request Prescription Refill',
+    RequestMedicalRecords = 'Request Medical Records',
+    GetLabResults = 'Get Test Results',
+    QuestionAboutBilling = 'Question About Billing',
+    QuestionAboutUpcomingSurgery = 'Question About Upcoming Surgery',
+    QuestionAboutSurgery = 'Question About Surgery',
+    ChatWithAgent = 'Talk With Live Agent',
+    ChatWithAnAgent = 'Talk With Live Agent',
+    AppointmentDetail = 'Appointment Details',
+    BookAppointment = 'Schedule Appointment',
+    CancelAppointment = 'Cancel Appointment',
+    RescheduleAppointment = 'Reschedule Appointment'
+}
