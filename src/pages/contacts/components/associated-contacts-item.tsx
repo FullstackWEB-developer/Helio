1c95f436fbd7ccb07fc97aac31378acc19cb93d0
Walter Huanaco <walter.huanaco@authoritypartners.com>
2022-05-16 06:25:28 +0000

    Bug 7722: Contacts: Company name is displayed for each associated contact

--- src/pages/contacts/components/associated-contacts-item.tsx
+++ src/pages/contacts/components/associated-contacts-item.tsx
@@ -14,7 +14,6 @@ const AsocciatedContactsItem = ({contact}: AsocciatedContactsItemProps) => {
         <div className='associated-contacts-item h-10 border-b contact-border-color-lighter body2 grid grid-cols-4 items-center'>
             <span>{`${contact.firstName} ${contact.lastName}`}</span>
             <span>{contact.jobTitle || contact.department || ''}</span>
-            <span>{contact.companyName}</span>
             <div className='justify-self-end pr-6 cursor-pointer'>
                 <SvgIcon type={Icon.View} fillClass='contact-light-fill' onClick={() => {history.push(`${ContactsPath}/${contact.id}`)}} />
             </div>
