c3d879e62c73c4668c100cc87f3f8d5e2bf1a9c0
Walter Huanaco <walter.huanaco@authoritypartners.com>
2022-05-31 16:07:34 +0000

    PBI 7684: Emails and notifications

--- src/pages/email/components/email-conversation/email-conversation.tsx
+++ src/pages/email/components/email-conversation/email-conversation.tsx
@@ -21,15 +21,17 @@ import ConversationHeader from '@components/conversation-header/conversation-hea
 import SendFirstEmail from '@pages/email/components/send-first-email/send-first-email';
 import {getContactById} from '@shared/services/contacts.service';
 import EmailReply from '@pages/email/components/email-message/email-reply';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Alert from '@components/alert/alert';
 import NewEmailHeader from '@pages/email/components/new-email/components/new-email-header';
 import './email-conversation.scss'
 import { BadgeValues } from '@pages/tickets/models/badge-values.model';
+import { selectUnreadEmails } from '@pages/email/store/email.selectors';
 
 const EmailConversation = () => {
     const {ticketId} = useParams<{ticketId: string}>();
     const dispatch = useDispatch();
+    const unreadEmailIds = useSelector(selectUnreadEmails);
     const [emailDisabledText, setEmailDisabledText] = useState<string>();
     const {data: ticket, refetch: refetchTicket} = useQuery([QueryTickets, ticketId], () => getTicketById(ticketId!), {
         enabled: !!ticketId
