259c56c881571825ea51dc50b1004f4412a7121b
Amir Lisovac <amir.lisovac@authoritypartners.com>
2022-09-01 07:22:57 +0000

    Bug 11447: Address display issue in registration form

--- src/pages/patients/components/summary/contact-information.tsx
+++ src/pages/patients/components/summary/contact-information.tsx
@@ -2,7 +2,7 @@ import {useSelector} from 'react-redux';
 import {selectPatient, selectPatientLoading} from '../../store/patients.selectors';
 import {useTranslation} from 'react-i18next';
 import PatientChartList from '@pages/patients/components/patient-chart-list';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import ContactInformationUpdate from '@pages/patients/components/summary/patient-contact-info-update';
 import withErrorLogging from '@shared/HOC/with-error-logging';
 import SvgIcon from '@components/svg-icon/svg-icon';
@@ -21,15 +21,25 @@ const ContactInformation = () => {
     const patientLoading = useSelector(selectPatientLoading);
 
     useEffect(() => {
-        setEditMode(false);        
+        setEditMode(false);
     }, [patientLoading])
-    
+
+    const constructAddressAppendWithApt = useCallback(() => {
+        let patientAddressLine = '';
+        if (patient.address) {
+            patientAddressLine += patient.address;
+        };
+        if (patient.address2) {
+            patientAddressLine += `, ${t(patient.address2.includes('#') ? 'patient.summary.apt_number_no_#' : 'patient.summary.apt_number')} ${patient.address2}`;
+        };
+        return patientAddressLine;
+    }, [patient]);
+
     const contactRows = [
-        { label: t('patient.summary.address'), values: [patient.address] },
-        { label: '', values: [patient.address2] },
-        { label: '', values: [`${patient?.city || ''}${patient?.state ? ', ': ''} ${patient.state || ''} ${patient.zip || ''}`] },
-        { label: t('patient.summary.email'), values: [patient.emailAddress], canEmail: !!patient.emailAddress },
-        { label: t('patient.summary.portal_access'), values: [t(booleanToText(patient.isPortalAccessGiven))] }
+        {label: t('patient.summary.address'), values: [constructAddressAppendWithApt()]},
+        {label: '', values: [`${patient?.city || ''}${patient?.state ? ', ' : ''} ${patient.state || ''} ${patient.zip || ''} `]},
+        {label: t('patient.summary.email'), values: [patient.emailAddress], canEmail: !!patient.emailAddress},
+        {label: t('patient.summary.portal_access'), values: [t(booleanToText(patient.isPortalAccessGiven))]}
     ];
 
     const contactSecondRows = [
@@ -48,13 +58,13 @@ const ContactInformation = () => {
             <div className='pb-1 pt-8 flex justify-between'>
                 <div className={'body1'}>{t('patient.summary.contact_information')} </div>
                 <SvgIcon type={Icon.Edit} onClick={() => setEditMode(!editMode)}
-                         className='icon-medium cursor-pointer mr-4'
-                         fillClass='edit-icon'/>
+                    className='icon-medium cursor-pointer mr-4'
+                    fillClass='edit-icon' />
             </div>
             {editMode ? <ContactInformationUpdate
-                onUpdateComplete={() => setEditMode(false)}/> : <div className='border-t grid grid-cols-2 gap-12'>
-                <PatientChartList headings={[]} rows={contactRows} patient={patient}/>
-                <PatientChartList headings={[]} rows={contactSecondRows} patient={patient}/>
+                onUpdateComplete={() => setEditMode(false)} /> : <div className='border-t grid grid-cols-2 gap-12'>
+                <PatientChartList headings={[]} rows={contactRows} patient={patient} />
+                <PatientChartList headings={[]} rows={contactSecondRows} patient={patient} />
             </div>
             }
         </div>
