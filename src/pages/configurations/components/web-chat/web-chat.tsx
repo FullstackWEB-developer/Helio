65699125e6f3c2daa7a03241b77956b05728f5bc
burak.bolek@authoritypartners.com-at-930184570490 <1CryPcwCAmkBWc4JNp8Js9HOI2PAlvaFtCbp5bVee14=>
2022-12-26 13:15:51 +0300

    Fix message

--- src/pages/configurations/components/web-chat/web-chat.tsx
+++ src/pages/configurations/components/web-chat/web-chat.tsx
@@ -239,8 +239,7 @@ const WebChat = () => {
                             <RouteLeavingGuard
                                 when={formState.isDirty && !formState.isSubmitSuccessful}
                                 navigate={path => history.push(path)}
-                                message={'common.confirm_close'}
-                                title={'configuration.general_settings.warning'}
+                                title={'configuration.practice_branding.warning_info_leaving'}
                             />
                             <Confirmation
                                 onClose={() => setWarning(false)}
